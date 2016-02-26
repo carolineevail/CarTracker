b3705cdb909a862dd29b5c7dbb63c8063f867c5a
carolineevail <carolineevail@gmail.com>
2016-02-26 11:19:58 -0500

    Initial import

---
+++ src/com/theironyard/Car.java
@@ -0,0 +1,11 @@
+package com.theironyard;
+
+/**
+ * Created by Caroline on 2/26/16.
+ */
+public class Car {
+    String make;
+    String model;
+    int modelYear;
+
+}
---
+++ src/com/theironyard/User.java
@@ -0,0 +1,7 @@
+package com.theironyard;
+
+/**
+ * Created by Caroline on 2/26/16.
+ */
+public class User {
+}
