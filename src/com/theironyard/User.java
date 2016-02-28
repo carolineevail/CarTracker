cd7c0fa87d0cb01f4aadf7b8d49a01ee93ff986f
carolineevail <carolineevail@gmail.com>
2016-02-28 16:29:48 -0500

    Expanded functionality in Main class

--- src/com/theironyard/User.java
+++ src/com/theironyard/User.java
@@ -7,8 +7,19 @@ import java.util.ArrayList;
  */
 public class User {
     String name;
+    int id;
+
+    public User(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
 
     public User(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
