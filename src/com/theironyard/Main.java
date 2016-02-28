9c97466aadef6b12db5da805630e7ff141051d46
carolineevail <carolineevail@gmail.com>
2016-02-28 16:28:47 -0500

    Expanded functionality in Main class

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,7 +1,9 @@
 package com.theironyard;
 
+import spark.ModelAndView;
 import spark.Session;
 import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,19 +16,63 @@ public class Main {
     public static void main(String[] args) {
 
         Spark.init();
-
         Spark.get(
                 "/",
                 ((request, response) -> {
-                    Session session = request.session();
-                    String userName = session.attribute("userName");
-                    if (userName == null) {
-                        users.put(userName, new User(userName));
+                    HashMap mustacheTags = new HashMap<>();
+                    String name = request.queryParams("loginName");
+                    if (name == null) {
+                        return new ModelAndView(mustacheTags, "login.html");
                     }
                     else {
-                        users.containsKey().
+                        Session session = request.session();
+                        User user = new User(name);
+                        users.put(name, user);
+                        mustacheTags.put("user", user);
+                        return new ModelAndView(mustacheTags, "home.html");
                     }
+
+                }),
+                new MustacheTemplateEngine()
+        );
+
+        Spark.post(
+                "/logout",
+                ((request, response) -> {
+                    Session session = request.session();
+                    session.invalidate();
+                    response.redirect("/");
+                    return "";
                 })
-        )
+        );
+        Spark.post(
+                "/create-entry",
+                ((request, response) -> {
+                    String carMake = request.queryParams("carMake");
+                    String carModel = request.queryParams("carModel");
+                    int carYear = Integer.valueOf(request.queryParams("carYear"));
+                    String carStyle = request.queryParams("carStyle");
+                    String carColor = request.queryParams("carColor");
+                    Car car = new Car(carMake, carModel, carYear, carStyle, carColor);
+
+                })
+        );
+//        Spark.post(
+//                "/create-entry",
+//                ((request, response) -> {
+//                    Session session = request.session();
+//                    String userName = session.attribute("userName");
+//                    if (userName == null) {
+//                        throw new Exception("Not logged in.");
+//                    }
+//
+//                    String make = request.queryParams("carMake");
+//                    String model = request.queryParams("carModel");
+//                    String year = request.queryParams("carYear");
+//
+//                })
+//        )
+
+
     }
 }
